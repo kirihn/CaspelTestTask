5769cb99943c910d7ceea8880dbbe3216faeb0e2
Zukay Kiryl <kzykov03@mail.ru>
2025-06-30 18:25:14 +0300

    feat: add sort colums and button for col modal window

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,30 +1,34 @@
 import { getRandomTableData } from '../../func/getRandomTableData';
-import { Table as TableAntd } from 'antd';
+import { Button, Table as TableAntd } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 import './table.scss';
 import { useState } from 'react';
 import { DataItem } from './types';
-import { DeleteTwoTone, EditTwoTone } from '@ant-design/icons';
+import { DeleteTwoTone, EditTwoTone, PlusOutlined } from '@ant-design/icons';
 
 export function Table() {
   const [tableData, setTableData] = useState<DataItem[]>(getRandomTableData(200));
 
+  
   const columns: ColumnsType<DataItem> = [
     {
       title: 'Имя',
       dataIndex: 'name',
       key: 'name',
+      sorter: (a, b) => a.name.localeCompare(b.name),
     },
     {
       title: 'Дата',
       dataIndex: 'date',
       key: 'date',
       render: (date: Date) => date.toLocaleString(),
+      sorter: (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime(),
     },
     {
       title: 'Числовое значение',
       dataIndex: 'value',
       key: 'value',
+      sorter: (a, b) => a.value - b.value,
     },
     {
       title: 'Действие',
@@ -44,7 +48,7 @@ export function Table() {
 
   return (
     <div className="tablePage">
-      <div className="tableParamsContainer"></div>
+      <div className="tableParamsContainer"><Button type='primary' shape='round' icon={<PlusOutlined />}>Добавить</Button></div>
       <div className="tableContainer">
         <TableAntd
           className="table"
