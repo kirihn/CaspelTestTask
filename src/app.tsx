2b7e3a0bccfcaaf7fe9ce5be2e14f2f9f1fdc81b
Zukay Kiryl <kzykov03@mail.ru>
2025-06-30 13:13:48 +0300

    feat: create project

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import './app.scss';
+export function App() {
+  return (
+    <>
+      <h1>Hello caspel!</h1>
+    </>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
+    clean: true,
+    publicPath: '',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  devServer: {
+    static: './dist',
+    port: 3000,
+    open: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'public/index.html' }),
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+  ],
+  mode: 'development',
+};
